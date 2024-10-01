0c456f5efe9aa65ce67a1d5962823cede4ce7362
Ephy Kimotho <ephyk48@gmail.com>
2024-10-01 22:49:30 +0300

    feat: built the Notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
-    <div>
-      <h1>Error 404 page goes here</h1>
-    </div>
+    <section className="min-h-screen flex flex-col justify-center items-center gap-10">
+      <h2 className="text-tomato font-extrabold font-karla text-4xl sm:text-5xl uppercase">
+        Page not found !
+      </h2>
+      <Link
+        to="/"
+        className="bg-darkBlue text-white font-roboto text-xl sm:text-2xl rounded-md py-4 px-6 font-bold uppercase"
+      >
+        Back to home
+      </Link>
+    </section>
   );
 }
 
